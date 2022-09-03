241e8bc7a4982d549746ec327576b56a8be31ec5
Anand raja <meetanandns@gmail.com>
2022-09-03 12:19:04 +0000

    calc working

--- main.js
+++ main.js
@@ -1,23 +1,103 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import { setupCounter } from './counter.js'
+import './src/style.css';
+import { onSetUpCalculator } from './src/initial_setup.js';
 
 document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
+  <main id="main">
+    <form id="calc_form">
+      <header class="calc_header">
+        <input
+          type="text"
+          disabled
+          id="output"
+          class="calc_output"
+          value="0"
+        />
+      </header>
+      <footer class="calc_footer">
+        <div class="btn_group">
+          <button
+            data-type="clear"
+            type="reset"
+            value="clear"
+            class="btn btn-grey"
+          >
+            AC
+          </button>
+          <button data-type="operator" value="invert" class="btn btn-grey">
+            +/-
+          </button>
+          <button data-type="operator" value="%" class="btn btn-grey">
+            %
+          </button>
+          <button data-type="operator" value="/" class="btn btn-orange">
+            ÷
+          </button>
+        </div>
+        <div class="btn_group">
+          <button
+            data-type="operand"
+            value="7"
+            class="btn btn-dark-grey"
+            id="7"
+          >
+            7
+          </button>
+          <button data-type="operand" value="8" class="btn btn-dark-grey">
+            8
+          </button>
+          <button data-type="operand" value="9" class="btn btn-dark-grey">
+            9
+          </button>
+          <button data-type="operator" value="*" class="btn btn-orange">
+            x
+          </button>
+        </div>
+        <div class="btn_group">
+          <button data-type="operand" value="4" class="btn btn-dark-grey">
+            4
+          </button>
+          <button data-type="operand" value="5" class="btn btn-dark-grey">
+            5
+          </button>
+          <button data-type="operand" value="6" class="btn btn-dark-grey">
+            6
+          </button>
+          <button data-type="operator" value="-" class="btn btn-orange">
+            -
+          </button>
+        </div>
+        <div class="btn_group">
+          <button data-type="operand" value="1" class="btn btn-dark-grey">
+            1
+          </button>
+          <button data-type="operand" value="2" class="btn btn-dark-grey">
+            2
+          </button>
+          <button data-type="operand" value="3" class="btn btn-dark-grey">
+            3
+          </button>
+          <button data-type="operator" value="+" class="btn btn-orange">
+            +
+          </button>
+        </div>
+        <div class="btn_group">
+          <button
+            data-type="operand"
+            value="0"
+            class="btn btn-grow btn-dark-grey"
+          >
+            0
+          </button>
+          <button data-type="operand" value="." class="btn btn-dark-grey">
+            .
+          </button>
+          <button value="=" data-type="operator" class="btn btn-orange">
+            =
+          </button>
+        </div>
+      </footer>
+    </form>
+  </main>
 `
 
-setupCounter(document.querySelector('#counter'))
+onSetUpCalculator();
